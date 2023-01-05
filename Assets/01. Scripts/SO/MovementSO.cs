390f7d102df534e8937b186af5483acdbe10e77a
SEH00N <06psh0501@gmail.com>
2023-01-05 14:34:58 +0900

    project init

--- Assets/01. Scripts/SO/MovementSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SO/MovementSO")]
-public class MovementSO : ScriptableObject
-{
-    [Range(0, 100f)] public float inAccel;
-    [Range(0, 100f)] public float deAccel;
-    [Range(0f, 30f)] public float maxSpeed;
-}
